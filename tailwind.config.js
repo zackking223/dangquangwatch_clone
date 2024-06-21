58e454daadd4ca0335c1f4f65fd27f377992fb47
Codenoob223 <vuongdepzai1262003@gmail.com>
2024-06-21 10:52:06 +0700

    Giao dien admin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/main/resources/templates/**/*.html'],
+  content: [
+    './src/main/resources/templates/**/*.html',
+    './src/main/resources/templates/fragments/*.html'
+  ],
   theme: {
     extend: {},
   },
