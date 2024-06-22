e788dce13203c525537f452e2542d5e3899905ea
Codenoob223 <vuongdepzai1262003@gmail.com>
2024-06-23 01:16:55 +0700

    Added horizontal scrolling for items lists

--- src/main/java/edu/it10/dangquangwatch/spring/Controller/IndexController.java
+++ src/main/java/edu/it10/dangquangwatch/spring/Controller/IndexController.java
@@ -1,7 +1,6 @@
 package edu.it10.dangquangwatch.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
