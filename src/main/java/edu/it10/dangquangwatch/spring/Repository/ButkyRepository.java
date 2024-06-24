1edbb34af129ba150b89dcb1cdba5462178852c4
Codenoob223 <vuongdepzai1262003@gmail.com>
2024-06-25 01:03:23 +0700

    Commit

--- src/main/java/edu/it10/dangquangwatch/spring/Repository/ButkyRepository.java
+++ src/main/java/edu/it10/dangquangwatch/spring/Repository/ButkyRepository.java
@@ -2,13 +2,19 @@ package edu.it10.dangquangwatch.spring.repository;
 
 import edu.it10.dangquangwatch.spring.entity.Butky;
 
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
   
 
 @Repository  
 public interface ButkyRepository extends JpaRepository<Butky, Integer> {
   Page<Butky> findByTenbutkyContains(String tenbutky, Pageable pageable);
+  
+  @Query("SELECT DISTINCT b FROM Butky b WHERE (UPPER(b.tenbutky) IN :upperValues OR UPPER(b.thongtin) IN :upperValues) ORDER BY b.NGAYTHEM DESC")
+  Page<Butky> searchButKy(@Param("upperValues") List<String> upperValues, Pageable pageable);
 }
--- src/main/java/edu/it10/dangquangwatch/spring/Service/impl/ButkyServiceImpl.java
+++ src/main/java/edu/it10/dangquangwatch/spring/Service/impl/ButkyServiceImpl.java
@@ -1,42 +1,50 @@
-package edu.it10.dangquangwatch.spring.service.impl;  
+package edu.it10.dangquangwatch.spring.service.impl;
 
-import edu.it10.dangquangwatch.spring.entity.Butky;  
-import edu.it10.dangquangwatch.spring.repository.ButkyRepository;  
-import edu.it10.dangquangwatch.spring.service.ButkyService;  
+import edu.it10.dangquangwatch.spring.entity.Butky;
+import edu.it10.dangquangwatch.spring.repository.ButkyRepository;
+import edu.it10.dangquangwatch.spring.service.ButkyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.stereotype.Service;  
+import org.springframework.stereotype.Service;
 
-import java.util.List;  
-import java.util.Optional;  
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Optional;
 
-@Service  
-public class ButkyServiceImpl implements ButkyService {  
-  @Autowired private ButkyRepository butkyRepository;  
+@Service
+public class ButkyServiceImpl implements ButkyService {
+  @Autowired
+  private ButkyRepository butkyRepository;
 
-  @Override  
-  public List<Butky> getAllButky() {  
-    return (List<Butky>) butkyRepository.findAll();  
-  }  
-
-  @Override  
-  public void saveButky(Butky butky) {  
-    butkyRepository.save(butky);  
-  }  
-
-  @Override  
-  public void deleteButky(Integer mabutky) {  
-    butkyRepository.deleteById(mabutky);  
-  }  
-
-  @Override  
-  public Optional<Butky> findButkyById(Integer mabutky) {  
-    return butkyRepository.findById(mabutky);  
+  @Override
+  public List<Butky> getAllButky() {
+    return (List<Butky>) butkyRepository.findAll();
   }
 
   @Override
-  public Page<Butky> getAllButkyByTenbutky(String tenbutky, Integer pageNum) {
-    return butkyRepository.findByTenbutkyContains(tenbutky, PageRequest.of(pageNum, 10));
-  }  
+  public void saveButky(Butky butky) {
+    butkyRepository.save(butky);
+  }
+
+  @Override
+  public void deleteButky(Integer mabutky) {
+    butkyRepository.deleteById(mabutky);
+  }
+
+  @Override
+  public Optional<Butky> findButkyById(Integer mabutky) {
+    return butkyRepository.findById(mabutky);
+  }
+
+  @Override
+  public Page<Butky> getAllButkyByTenbutky(String searchStr, Integer pageNum) {
+    List<String> values = new ArrayList<String>(Arrays.asList(searchStr.split(" , ")));
+    List<String> upperValues = values.stream()
+        .map(String::toUpperCase)
+        .collect(Collectors.toList());
+    return butkyRepository.searchButKy(upperValues, PageRequest.of(pageNum, 10));
+  }
 }
