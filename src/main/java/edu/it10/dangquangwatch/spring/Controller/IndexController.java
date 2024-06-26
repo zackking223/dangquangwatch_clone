90e3805ebe256383089fc3df3baf02bcf6e5589f
NBo230523 <nguyenbo12a1@gmail.com>
2024-06-27 01:08:53 +0700

    title bar

--- src/main/java/edu/it10/dangquangwatch/spring/Controller/IndexController.java
+++ src/main/java/edu/it10/dangquangwatch/spring/Controller/IndexController.java
@@ -3,10 +3,10 @@ package edu.it10.dangquangwatch.spring.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.it10.dangquangwatch.spring.entity.Butky;
@@ -23,8 +23,6 @@ import jakarta.websocket.server.PathParam;
 
 import org.springframework.web.bind.annotation.PostMapping;
 
-
-@RequestMapping("/")
 @Controller
 public class IndexController {
     @Autowired
@@ -39,65 +37,176 @@ public class IndexController {
     @Autowired
     private ButkyService butkyService;
 
-    @GetMapping("dongho/{id}")
+    @GetMapping("/dongho/{id}")
     public String trangDongHo(Model model, @PathParam("id") String madongho) {
         int id = convertToNum(madongho);
         Optional<Dongho> donghoEdit = donghoService.findDonghoById(id);
         donghoEdit.ifPresent(dongho -> model.addAttribute("dongho", dongho));
         donghoEdit.ifPresent(dongho -> model.addAttribute("title", dongho.getTendongho()));
-        
+
         return "dongho";
     }
 
-    @GetMapping("phukien/{id}")
+    @GetMapping("/phukien/{id}")
     public String trangPhuKien(Model model, @PathParam("id") String maphukien) {
         int id = convertToNum(maphukien);
         Optional<PhuKien> phukienEdit = phuKienService.findPhuKienById(id);
         phukienEdit.ifPresent(phukien -> model.addAttribute("phukien", phukien));
         phukienEdit.ifPresent(phukien -> model.addAttribute("title", phukien.getTenPhuKien()));
-        
+
         return "phukien";
     }
 
-    @GetMapping("butky/{id}")
+    @GetMapping("/butky/{id}")
     public String trangButKy(Model model, @PathParam("id") String mabutky) {
         int id = convertToNum(mabutky);
 
         Optional<Butky> butkyEdit = butkyService.findButkyById(id);
         butkyEdit.ifPresent(butky -> model.addAttribute("butky", butky));
         butkyEdit.ifPresent(butky -> model.addAttribute("title", butky.getTenbutky()));
-        
+
         return "butky";
     }
 
-    @GetMapping("kinhmat/{id}")
+    @GetMapping("/kinhmat/{id}")
     public String trangKinhMat(Model model, @PathParam("id") String makinhmat) {
         int id = convertToNum(makinhmat);
 
         Optional<KinhMat> kinhmatEdit = kinhMatService.findKinhMatById(id);
         kinhmatEdit.ifPresent(kinhmat -> model.addAttribute("kinhmat", kinhmat));
         kinhmatEdit.ifPresent(kinhmat -> model.addAttribute("title", kinhmat.getTenSanPham()));
-        
+
         return "kinhmat";
     }
 
-    @GetMapping("trangsuc/{id}")
+    @GetMapping("/trangsuc/{id}")
     public String trangTrangSuc(Model model, @PathParam("id") String matrangsuc) {
         int id = convertToNum(matrangsuc);
 
         Optional<Trangsuc> trangsucEdit = trangsucService.findTrangsucById(id);
         trangsucEdit.ifPresent(trangsuc -> model.addAttribute("trangsuc", trangsuc));
         trangsucEdit.ifPresent(trangsuc -> model.addAttribute("title", trangsuc.getTentrangsuc()));
-        
+
         return "trangsuc";
+    }
+
+    @GetMapping("/butky")
+    public String trangDanhSachButKy(@RequestParam("search") Optional<String> search,
+            @RequestParam("page") Optional<Integer> page,
+            Model model) {
+        Page<Butky> data;
+        String searchStr = "";
+        int pageNum = 0;
+
+        if (search.isPresent())
+            searchStr = search.get();
+        if (page.isPresent())
+            pageNum = page.get() - 1;
+        data = butkyService.searchButky(searchStr, pageNum);
+
+        model.addAttribute("butkys", data.getContent());
+        model.addAttribute("page", pageNum);
+        model.addAttribute("search", searchStr);
+        model.addAttribute("sotrang", data.getTotalPages());
+
+        return "danhsach/butky/search";
+    }
+
+    @GetMapping("/dongho")
+    public String trangDanhSachDongHo(@RequestParam("search") Optional<String> search,
+            @RequestParam("page") Optional<Integer> page,
+            Model model) {
+        Page<Dongho> data;
+        String searchStr = "";
+        int pageNum = 0;
+        Dongho fieldData = new Dongho();
+
+        if (search.isPresent())
+            searchStr = search.get();
+        if (page.isPresent())
+            pageNum = page.get() - 1;
+        data = donghoService.getAllDonghoByTendongho(searchStr, pageNum);
+
+        model.addAttribute("dongho", fieldData);
+        model.addAttribute("donghos", data.getContent());
+        model.addAttribute("page", pageNum);
+        model.addAttribute("search", searchStr);
+        model.addAttribute("sotrang", data.getTotalPages());
+        return "danhsach/dongho/search";
+    }
+
+    @GetMapping("/kinhmat")
+    public String trangDanhSachKinhMat(@RequestParam("search") Optional<String> search,
+            @RequestParam("page") Optional<Integer> page,
+            Model model) {
+        Page<KinhMat> data;
+        String searchStr = "";
+        int pageNum = 0;
+
+        if (search.isPresent())
+            searchStr = search.get();
+        if (page.isPresent())
+            pageNum = page.get() - 1;
+        data = kinhMatService.searchKinhMat(searchStr, pageNum);
+
+        model.addAttribute("kinhmats", data.getContent());
+        model.addAttribute("page", pageNum);
+        model.addAttribute("search", searchStr);
+        model.addAttribute("sotrang", data.getTotalPages());
+
+        return "danhsach/kinhmat/search";
+    }
+
+    @GetMapping("/phukien")
+    public String trangDanhSachPhuKien(@RequestParam("search") Optional<String> search,
+            @RequestParam("page") Optional<Integer> page,
+            Model model) {
+        Page<PhuKien> data;
+        String searchStr = "";
+        int pageNum = 0;
+
+        if (search.isPresent())
+            searchStr = search.get();
+        if (page.isPresent())
+            pageNum = page.get() - 1;
+        data = phuKienService.searchPhuKien(searchStr, pageNum);
+
+        model.addAttribute("phukiens", data.getContent());
+        model.addAttribute("page", pageNum);
+        model.addAttribute("search", searchStr);
+        model.addAttribute("sotrang", data.getTotalPages());
+
+        return "danhsach/phukien/search";
+    }
+
+    @GetMapping("/trangsuc")
+    public String trangDanhSachTrangSuc(@RequestParam("search") Optional<String> search,
+            @RequestParam("page") Optional<Integer> page,
+            Model model) {
+        Page<Trangsuc> data;
+        String searchStr = "";
+        int pageNum = 0;
+
+        if (search.isPresent())
+            searchStr = search.get();
+        if (page.isPresent())
+            pageNum = page.get() - 1;
+        data = trangsucService.searchTrangsuc(searchStr, pageNum);
+
+        model.addAttribute("trangsucs", data.getContent());
+        model.addAttribute("page", pageNum);
+        model.addAttribute("search", searchStr);
+        model.addAttribute("sotrang", data.getTotalPages());
+
+        return "danhsach/trangsuc/search";
     }
 
     @PostMapping("/")
     public String postIndex() {
         return "redirect: /";
     }
-    
-    @GetMapping
+
+    @GetMapping("/")
     public String index(@RequestParam("search") Optional<String> search, @RequestParam("page") Optional<Integer> page,
             Model model) {
         if (search.isPresent()) {
@@ -134,7 +243,7 @@ public class IndexController {
         return "index";
     }
 
-    @GetMapping("error")
+    @GetMapping("/error")
     public String errorRedirect() {
         return "redirect: /";
     }
