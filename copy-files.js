19011918bcac60e00ac40daac62159c2985ef2a7
Codenoob223 <vuongdepzai1262003@gmail.com>
2024-12-15 08:03:35 +0700

    Feat: Fix ENOENT error by checking if folder exist

--- copy-files.js
+++ copy-files.js
@@ -1,9 +1,16 @@
 var ncp = require('ncp').ncp;
 var fs = require('fs');
+var path = require('path');
+
+// Đảm bảo thư mục target/classes tồn tại
+var targetDir = path.join('target', 'classes');
+if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir, { recursive: true });
+}
 
 ncp.limit = 16;
 
-ncp('./src/main/resources', 'target/classes', {
+ncp('./src/main/resources', targetDir, {
     filter: (source) => {
         if (fs.lstatSync(source).isDirectory()) {
             return true;
@@ -15,4 +22,5 @@ ncp('./src/main/resources', 'target/classes', {
     if (err) {
         return console.error(err);
     }
+    console.log('Copy completed successfully!');
 });
