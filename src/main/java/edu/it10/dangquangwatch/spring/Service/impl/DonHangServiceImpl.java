0becdbdbb1e590b43395ed3bd9b875fc228f52ad
Codenoob223 <vuongdepzai1262003@gmail.com>
2025-01-02 16:26:51 +0700

    Feat: Added pessimistic locking

--- src/main/java/edu/it10/dangquangwatch/spring/Service/impl/DonHangServiceImpl.java
+++ src/main/java/edu/it10/dangquangwatch/spring/Service/impl/DonHangServiceImpl.java
@@ -378,34 +378,35 @@ public class DonHangServiceImpl implements DonHangService {
     }
   }
 
+  @Transactional
   void clearTempAmount(ChiTietDonHang item) {
     switch (item.getLoaiSanPham()) {
       case "dongho":
-        Dongho dongHo = donghoService.findById(item.getMaSanPham())
+        Dongho dongHo = donghoService.findByIdWithLock(item.getMaSanPham())
             .orElseThrow(() -> new OrderException("Đồng hồ không tồn tại, mã: " + item.getMaSanPham()));
         dongHo.setSoluongdatmua(dongHo.getSoluongdatmua() - item.getSoLuong());
         donghoService.save(dongHo);
         break;
       case "phukien":
-        PhuKien phuKien = phuKienService.findById(item.getMaSanPham())
+        PhuKien phuKien = phuKienService.findByIdWithLock(item.getMaSanPham())
             .orElseThrow(() -> new OrderException("Phụ kiện không tồn tại, mã: " + item.getMaSanPham()));
         phuKien.setSoluongdatmua(phuKien.getSoluongdatmua() - item.getSoLuong());
         phuKienService.save(phuKien);
         break;
       case "kinhmat":
-        KinhMat kinhMat = kinhMatService.findById(item.getMaSanPham())
+        KinhMat kinhMat = kinhMatService.findByIdWithLock(item.getMaSanPham())
             .orElseThrow(() -> new OrderException("Kính mắt không tồn tại, mã: " + item.getMaSanPham()));
         kinhMat.setSoluongdatmua(kinhMat.getSoluongdatmua() - item.getSoLuong());
         kinhMatService.save(kinhMat);
         break;
       case "butky":
-        Butky butKy = butKyService.findById(item.getMaSanPham())
+        Butky butKy = butKyService.findByIdWithLock(item.getMaSanPham())
             .orElseThrow(() -> new OrderException("Bút ký không tồn tại, mã: " + item.getMaSanPham()));
         butKy.setSoluongdatmua(butKy.getSoluongdatmua() - item.getSoLuong());
         butKyService.save(butKy);
         break;
       case "trangsuc":
-        Trangsuc trangSuc = trangSucService.findById(item.getMaSanPham())
+        Trangsuc trangSuc = trangSucService.findByIdWithLock(item.getMaSanPham())
             .orElseThrow(() -> new OrderException("Trang sức không tồn tại, mã: " + item.getMaSanPham()));
         trangSuc.setSoluongdatmua(trangSuc.getSoluongdatmua() - item.getSoLuong());
         trangSucService.save(trangSuc);
